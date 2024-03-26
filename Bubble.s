ec7ee7b6ed8b3eb03623afba656b4791ab75bea0
Bri1l <brodyearley@icloud.com>
2024-03-26 16:25:25 +0000

    -Program can now print an array- -Fixed exit syscall- -Fixed looping problem by changing offset register to ebx from eax- -Integrated incoffset into sorter- -Added a newline printer-

--- Bubble.s
+++ Bubble.s
@@ -1,5 +1,6 @@
 section .rodata
     arr DB 3, 4, 7, 1, 6, 0 ; Unsorted array
+    newline: db 0x0a ; newline character
 
 section .bss
     ; Stores int result and ASCII result
@@ -11,35 +12,41 @@ global _start
 section .text
 
 _start:
-    mov eax, 0 ; Base offset value
+    mov ebx, 0 ; Base offset value, must use and index register like ebx, eax doesnt work
     call sorter
 
-    mov rax, 1       ; Exit
-    mov rdi, 0        ; Exit success
-    syscall           ; Call kernel
+    mov rax, 60 ; Exit
+    mov rdi, 0 ; Exit success
+    syscall ; Call kernel
 
 ; Sorts numbs, does the heavy lifting
 sorter:
-    mov ebx, [arr + eax] ; Moves mem address arr[0] + offset to ebx
-    mov [result], ebx ; Stores this in result
-    call itoc ; Call int print function
-    cmp ebx, 0 ; Checks if current array element == 0
-    jne incoffset ; Runs sorter again if this is not the case
+    mov al, byte [arr + ebx] ; Moves element arr[offset] into ebx
+    mov [result], al ; Stores this in result, will be used after itoc
+    call itoc ; Call int print function. Overwrites registers and zero flag
+    inc ebx ; Increments index. Cannot go after cmp, modifies zero flag
+    mov al, [result] ; Copies result back into al
+    ; Checks if current array element == 0. Restarts sorter if element != 0
+    cmp byte al, 0
+    jne sorter
     ret
-
-incoffset:
-    inc eax ; Increment offset
-    jmp sorter
 
 ; Integer to Character. Prints ints. Can only accept single integers. 1=Good, 15=Bad
 itoc:
-    mov al, 0x30 ; Moves 0x30 into memory, ASCII for 0
-    add al, [result] ; Add integer to 0x30
+    add al, 0x30 ; Adds 0x30 (ASCII for 0) to result
     mov [resprint], al ; Returns this calculation to resprint
 
-    mov rax, 1        ; Write mode
-    mov rdi, 1        ; STDOUT
+    ; Prints numeric character
+    mov rax, 1 ; Write mode
+    mov rdi, 1 ; STDOUT
     mov rsi, resprint ; address of resprint
-    mov rdx, 1        ; length of resprint
-    syscall           ; Call kernel
+    mov rdx, 1 ; length of resprint
+    syscall ; Call kernel
+
+    ; Prints newline
+    mov rax, 1 ; Write mode
+    mov rdi, 1 ; STDOUT
+    mov rsi, newline ; address of newline
+    mov rdx, 1 ; length of newline
+    syscall ; Call kernel
     ret
