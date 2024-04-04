4e0dd571a526af70381704951c0e8d3435f76a44
Bri1l <brodyearley@icloud.com>
2024-04-04 17:27:35 +0100

    Sorter works now, probably should have made more commits, but it wasnt working for most of the time so I didnt bother. -Sorts integers through a bubble sort algorithm- -Checks if al is bigger than ah and vice versa- -Had a problem were rcx wasnt being popped when itoc was called, added 'Popper' to fix that- -Added a checker function to iterate through sArr to check if the array is sorted- -Added uArrSize variable which checks the size of the initial array before running- -Stopped 0 (sentinel value) being operated on by changing 'uArrSize - 1' to 'uArrSize - 2'- -Added 'initsArr' function which initialises sArr with uArr data- -Removed variable 'result', was unecessary and never used-

--- Bubble.s
+++ Bubble.s
@@ -1,38 +1,102 @@
 section .rodata
-    arr DB 3, 4, 7, 1, 6, 0 ; Unsorted array
-    newline: db 0x0a ; newline character
+    uarr DB 3, 4, 7, 1, 6, 0 ; Unsorted array
+    uarrSize equ $-uarr ; Gets size of uarr
+    newline: db 0x0a ; Newline character
 
 section .bss
-    ; Stores int result and ASCII result
-    result resb 1
-    resprint resb 1
+    resprint resb 1 ; Stores ASCII result to print
+    sArr resb uarrSize ; New array, uarr cannot be operated on, this keeps data intact
 
 global _start
 
 section .text
 
 _start:
-    mov ebx, 0 ; Base offset value, must use and index register like ebx, eax doesnt work
-    call sorter
+    mov ecx, 0 ; initsArr offset value, must use and index register like ecx, eax doesnt work
+    call initsArr
+    call Sorter
 
     mov rax, 60 ; Exit
     mov rdi, 0 ; Exit success
     syscall ; Call kernel
 
-; Sorts numbs, does the heavy lifting
-sorter:
-    mov al, byte [arr + ebx] ; Moves element arr[offset] into ebx
-    mov [result], al ; Stores this in result, will be used after itoc
-    call itoc ; Call int print function. Overwrites registers and zero flag
-    inc ebx ; Increments index. Cannot go after cmp, modifies zero flag
-    mov al, [result] ; Copies result back into al
-    ; Checks if current array element == 0. Restarts sorter if element != 0
-    cmp byte al, 0
-    jne sorter
+; Initialises sArr with 0's
+initsArr:
+    ; Move element from uarr into sArr
+    mov al, [uarr + ecx]
+    mov [sArr + ecx], al
+    inc ecx ; Increment loop index
+    ; Check that all data has been copied, if not, then restart, if so, then return to _start
+    cmp ecx, uarrSize - 1
+    jl initsArr
+    mov ecx, -1 ; Sorter base offset value, -1 cause Sorter performs increment
     ret
+
+; Sorts numbs, does the heavy lifting
+Sorter:
+    inc ecx ; Before cmp, overwrites zero flag
+    ; Check index does not surpass array, if so, reset index
+    cmp ecx, uarrSize - 1
+    jge OffReset
+    mov al, byte [sArr + ecx] ; Moves element arr[offset] into al
+    mov ah, byte [sArr + ecx + 1] ; Moves element arr[offset + 1] into ah
+
+    push rcx ; ecx to be used in Checker, so this index data is saved
+    ; Checks if sentinel value has been reached, if so, then go straight to checker and dont move it
+    cmp ah, 0
+    je CheckerInit
+    ; Compare al and ah, then jump to a function depending on this result
+    cmp al, ah
+    je CheckerInit
+    jg Bigger
+    jl Smaller
+
+sortRet:
+    ret ; returns back from last function called to stack, in this case: the sorter call in _start
+
+; If al is bigger than ah
+Bigger:
+    mov [sArr + ecx + 1], al
+    mov [sArr + ecx], ah
+    jmp CheckerInit ; Unconditional jump
+
+; If al is smaller than ah. Probably redundant due to initialisation. CHECK
+Smaller:
+    mov [sArr + ecx + 1], ah
+    mov [sArr + ecx], al
+    jmp CheckerInit ; Unconditional jump
+
+; Checks if sArr is in correct order
+Checker:
+    cmp ecx, uarrSize - 2 ; Checks if index reached limit (-2 cause -1 is sentinel), if so, program is successful & prints
+    je Popper ; Go to popper first because RAX data is offsetting the stack, this will affect the return call later
+    mov al, [sArr + ecx] ; Move sArr[ecx] into al
+    mov ah, [sArr + ecx + 1] ; Move sArr[ecx + 1] into ah
+    inc ecx ; Increment loop
+    ; If al (first index) is smaller than ah (second index), then restart loop
+    cmp al, ah
+    jl Checker
+    pop rcx ; Give ecx its data back
+    jmp Sorter ; Unconditional jump
+
+; Resets offset (index)
+OffReset:
+    mov ecx, -1 ; Will be incremented to 0 in sorter
+    jmp Sorter
+
+CheckerInit:
+    mov ecx, 0
+    jmp Checker
+
+; Give ecx its data back so as to not offset the stack, then go to itoc
+Popper:
+    pop rcx
+    mov ebx, 0 ; For itoc offset
+    jmp itoc
 
 ; Integer to Character. Prints ints. Can only accept single integers. 1=Good, 15=Bad
 itoc:
+    mov al, [sArr + ebx] ; Move value into al
     add al, 0x30 ; Adds 0x30 (ASCII for 0) to result
     mov [resprint], al ; Returns this calculation to resprint
 
@@ -49,4 +113,8 @@ itoc:
     mov rsi, newline ; address of newline
     mov rdx, 1 ; length of newline
     syscall ; Call kernel
-    ret
+
+    cmp ebx, uarrSize - 2 ; "- 2" Stops printing of 0
+    je sortRet ; If print finished, program successful, then exit program
+    inc ebx ; Increment printing index
+    jmp itoc ; Unconditional jump if nothing is met to continue printing
